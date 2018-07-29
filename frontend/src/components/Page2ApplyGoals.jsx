8c0c1dd5363485f48331a9cec233f95bfc5647cc
xonix <xonixx@gmail.com>
2018-07-29 10:24:54 -0700

    impr

--- frontend/src/components/Page2ApplyGoals.jsx
+++ frontend/src/components/Page2ApplyGoals.jsx
@@ -165,6 +165,11 @@ class Page2ApplyGoals extends Component {
 
                 &nbsp;&nbsp;&nbsp;&nbsp;
                 <label><input type={'radio'} name={'preset'}
+                              checked={this.state.preset === healthiest}
+                              onChange={this.selectPreset.bind(this, healthiest)}/> healthiest</label>
+
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === fastest}
                               onChange={this.selectPreset.bind(this, fastest)}/> fastest</label>
 
@@ -172,12 +177,6 @@ class Page2ApplyGoals extends Component {
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === cheapest}
                               onChange={this.selectPreset.bind(this, cheapest)}/> cheapest</label>
-
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <label><input type={'radio'} name={'preset'}
-                              checked={this.state.preset === healthiest}
-                              onChange={this.selectPreset.bind(this, healthiest)}/> healthiest</label>
-
 
                 {
                     this.state.preset
