40a4bd3d422e5f0da883469fc22f3dcfddff3370
xonix <xonixx@gmail.com>
2018-07-28 23:19:11 -0700

    presets

--- frontend/src/components/Page2ApplyGoals.jsx
+++ frontend/src/components/Page2ApplyGoals.jsx
@@ -5,20 +5,47 @@ import 'rc-slider/assets/index.css';
 
 const SliderT = Slider.createSliderWithTooltip(Slider);
 
+const PRESETS = {
+    'fastest': {
+        'Price': 90,
+        'Calories': 10,
+        'Distance': 20
+    },
+    'cheapest': {
+        'Price': 20,
+        'Calories': 70,
+        'Distance': 70
+    },
+    'healthiest': {
+        'Price': 50,
+        'Calories': 90,
+        'Distance': 10
+    },
+    'balanced': {
+        'Price': 50,
+        'Calories': 50,
+        'Distance': 50
+    },
+};
+
 class Page2ApplyGoals extends Component {
     state = {
         preset: ''
     };
 
     slider = (title, value) => {
-        return <table style={{width: '60%', margin: '0 auto'}}>
+        return <table key={title} style={{width: '60%', margin: '0 auto'}}>
             <tbody>
             <tr>
                 <td style={{width: '20%'}}>{title}</td>
-                <td style={{width: '80%'}}><SliderT/></td>
+                <td style={{width: '80%'}}><SliderT value={value}/></td>
             </tr>
             </tbody>
         </table>
+    };
+
+    selectPreset = (preset) => {
+        this.setState({preset: preset, presetValues: PRESETS[preset]})
     };
 
     render() {
@@ -28,26 +55,33 @@ class Page2ApplyGoals extends Component {
 
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === 'fastest'}
-                              onChange={() => this.setState({preset: 'fastest'})}/> fastest</label>
+                              onChange={this.selectPreset.bind(this, 'fastest')}/> fastest</label>
                 {' '}
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === 'cheapest'}
-                              onChange={() => this.setState({preset: 'cheapest'})}/> cheapest</label>
+                              onChange={this.selectPreset.bind(this, 'cheapest')}/> cheapest</label>
 
                 {' '}
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === 'healthiest'}
-                              onChange={() => this.setState({preset: 'healthiest'})}/> healthiest</label>
+                              onChange={this.selectPreset.bind(this, 'healthiest')}/> healthiest</label>
 
                 {' '}
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === 'balanced'}
-                              onChange={() => this.setState({preset: 'balanced'})}/> balanced</label>
+                              onChange={this.selectPreset.bind(this, 'balanced')}/> balanced</label>
 
-                {this.slider('Price')}
-                {this.slider('Calories')}
-                {this.slider('Distance')}
-                <br/>
+                {
+                    this.state.preset
+                    &&
+                    <div>
+                        {
+                            ['Price', 'Calories', 'Distance'].map(p =>
+                                this.slider(p, this.state.presetValues[p])
+                            )
+                        }
+                    </div>
+                }
                 <Link to={'/page3'}>Next</Link>
             </div>
         );
