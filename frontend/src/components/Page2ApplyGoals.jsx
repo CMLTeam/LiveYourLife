61c49874597a1bb85688f57e094809f9a1f8f1fa
xonix <xonixx@gmail.com>
2018-07-28 23:05:20 -0700

    goals : sliders

--- frontend/src/components/Page2ApplyGoals.jsx
+++ frontend/src/components/Page2ApplyGoals.jsx
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import Slider, {Range} from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+const SliderT = Slider.createSliderWithTooltip(Slider);
 
 class Page2ApplyGoals extends Component {
     state = {
         preset: ''
-    }
+    };
+
+    slider = (title, value) => {
+        return <table style={{width: '60%', margin: '0 auto'}}>
+            <tbody>
+            <tr>
+                <td style={{width: '20%'}}>{title}</td>
+                <td style={{width: '80%'}}><SliderT/></td>
+            </tr>
+            </tbody>
+        </table>
+    };
 
     render() {
         return (
@@ -22,13 +37,16 @@ class Page2ApplyGoals extends Component {
                 {' '}
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === 'healthiest'}
-                                  onChange={() => this.setState({preset: 'healthiest'})}/> healthiest</label>
+                              onChange={() => this.setState({preset: 'healthiest'})}/> healthiest</label>
 
                 {' '}
                 <label><input type={'radio'} name={'preset'}
                               checked={this.state.preset === 'balanced'}
-                                      onChange={() => this.setState({preset: 'balanced'})}/> balanced</label>
+                              onChange={() => this.setState({preset: 'balanced'})}/> balanced</label>
 
+                {this.slider('Price')}
+                {this.slider('Calories')}
+                {this.slider('Distance')}
                 <br/>
                 <Link to={'/page3'}>Next</Link>
             </div>
