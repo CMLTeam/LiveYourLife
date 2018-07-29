2ee4b14402d191503236f39377b3560ffbaed4ab
xonix <xonixx@gmail.com>
2018-07-28 22:53:31 -0700

    goals

--- frontend/src/components/Page2ApplyGoals.jsx
+++ frontend/src/components/Page2ApplyGoals.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 
 class Page2ApplyGoals extends Component {
+    state = {
+        preset: ''
+    }
+
     render() {
         return (
             <div>
-                Apply Goals
+                <h2>Apply Goals</h2>
+
+                <label><input type={'radio'} name={'preset'}
+                              checked={this.state.preset === 'fastest'}
+                              onChange={() => this.setState({preset: 'fastest'})}/> fastest</label>
+                {' '}
+                <label><input type={'radio'} name={'preset'}
+                              checked={this.state.preset === 'cheapest'}
+                              onChange={() => this.setState({preset: 'cheapest'})}/> cheapest</label>
+
+                {' '}
+                <label><input type={'radio'} name={'preset'}
+                              checked={this.state.preset === 'healthiest'}
+                                  onChange={() => this.setState({preset: 'healthiest'})}/> healthiest</label>
+
+                {' '}
+                <label><input type={'radio'} name={'preset'}
+                              checked={this.state.preset === 'balanced'}
+                                      onChange={() => this.setState({preset: 'balanced'})}/> balanced</label>
 
                 <br/>
                 <Link to={'/page3'}>Next</Link>
@@ -13,4 +35,5 @@ class Page2ApplyGoals extends Component {
         );
     }
 }
+
 export default Page2ApplyGoals;
