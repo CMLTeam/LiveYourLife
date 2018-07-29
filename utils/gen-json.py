39958e1ceaa4de845c1b94299ec9789171f20272
Vasily Tarasov <tarasov@vasily.name>
2018-07-29 02:06:02 -0700

    Computing all trajectories (permutations) and their costs, calories, durations

--- utils/gen-json.py
+++ utils/gen-json.py
@@ -1,6 +1,7 @@
 import googlemaps
 import json
 import urllib2
+import itertools
 
 apikey="AIzaSyCpPfnNhtN1ecsNLedF7GlfmPdLhlhqUm4"
 gmaps = googlemaps.Client(key=apikey)
@@ -83,12 +84,57 @@ def print_sdpair(sdpair):
         ti = ti + 1
 
 sdpairs = []
-
 for i in range(len(locs) - 1):
     sdpair = get_trips(locs[i], locs[i + 1])
     print_sdpair(sdpair)
     print "------------------------------------------"
     sdpairs.append(sdpair)
+
+
+
+# generate all possible trajectories (permutations of Trips)
+l = []
+for sdpair in sdpairs:
+    l.append(range(0, len(sdpair["trips"])))
+perms = [s for s in itertools.product(*l)]
+
+print perms
+
+trajs = []
+for perm in perms:
+    traj = {}
+    traj["trips"] = []
+    for (sdpair, trip) in zip(sdpairs, perm):
+        #traj.append(sdpair["trips"][trip]["statistics"]["distance_km"])
+        traj["trips"].append(sdpair["trips"][trip])
+    trajs.append(traj)
+
+print len(trajs)
+             
+# ok, now let's iterate over trajs and see their distances
+
+i = 1
+for traj in trajs:
+    print "Traj " + str(i)
+    j = 0
+    traj_cost = 0
+    traj_cal = 0
+    traj_time = 0
+    for trip in traj["trips"]:
+        trip_time, trip_cost, trip_cal = calc_trip_cost(trip)
+        traj_cost = traj_cost + trip_cost
+        traj_cal = traj_cal + trip_cal
+        traj_time = traj_time + trip_time
+        #print "  Trip " + str(j) + ": " + str(trip["statistics"]["distance_km"])
+        j = j + 1
+    print "  " + str(traj_time) + "sec " + str(traj_cost) + "$ " + str(traj_cal) + "cal"
+    traj["duration"] = traj_time
+    traj["cost"] = traj_cost
+    traj["cal"] = traj_cal
+    i = i + 1
+
+print json.dumps(trajs, indent=4, sort_keys=True)
+
 
 #------------------------------------------------------------------------
 
